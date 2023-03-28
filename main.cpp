5ef32da514646e17874f928cee16f7bf3ea29156
dbatt-dev <128836567+dbatt-dev@users.noreply.github.com>
2023-03-28 09:08:36 -0700

    Update main.cpp
    Added added an enumeration

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 //using namespace std;
 int main() {
+    const int weight_goal =100;
+    enum MONTH {kJan=0, kFeb} start_month;
+    MONTH end_month = kFeb;
     int i = 0;
-    std::cout << sizeof(int) << std::endl;
+    if (end_month == kFeb){
+        std::cout << sizeof(int) << std::endl;
+    }
     std::cout << i << "\n";
+    std::cout.flush();
     return 0;
 }
